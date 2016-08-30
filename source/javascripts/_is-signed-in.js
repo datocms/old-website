31734ff94bde13e1be3193ae9ddae28e9e6cde3c
Stefano Verna <s.verna@cantierecreativo.net>
2016-08-30 12:00:35 +0200

    Simplify using cookies

--- source/javascripts/_is-signed-in.js
+++ source/javascripts/_is-signed-in.js
@@ -1,39 +1,7 @@
-var iframe = document.createElement("iframe");
-var dashboardBaseUrl = "https://dashboard.datocms.com";
+var elementsToShow = document.cookie.match(/isSignedIn/) ?
+  "[role=dashboard-link]" :
+  "[role=sign-in-link], [role=register-link]";
 
-iframe.setAttribute("src", dashboardBaseUrl + "/?disableStart=1");
-iframe.style.width = "1px";
-iframe.style.height = "1px";
-iframe.style.opacity = "0";
-document.body.appendChild(iframe);
-
-iframe.onload = function() {
-  iframe.contentWindow.postMessage(
-    JSON.stringify({ "command": "isSignedIn" }),
-    dashboardBaseUrl
-  );
-};
-
-window.addEventListener("message", function(e) {
-  var origin = e.origin || e.originalEvent.origin;
-
-  if (origin !== dashboardBaseUrl) {
-    return;
-  }
-
-  var message = JSON.parse(e.data);
-
-  if (message.command === "isSignedInReply") {
-    console.log(message.value);
-
-    var elementsToShow = message.value ?
-      "[role=dashboard-link]" :
-      "[role=sign-in-link], [role=register-link]";
-
-    [].forEach.call(document.querySelectorAll(elementsToShow), function(el) {
-      el.removeAttribute("style");
-    });
-  }
-
-  iframe.parentNode.removeChild(iframe);
-}, false);
+[].forEach.call(document.querySelectorAll(elementsToShow), function(el) {
+  el.removeAttribute("style");
+});
