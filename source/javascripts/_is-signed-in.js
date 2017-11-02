8a522182ce742a87b06346159f19c4d679d83072
Stefano Verna <s.verna@cantierecreativo.net>
2017-11-02 11:46:35 +0100

    Show account email

--- source/javascripts/_is-signed-in.js
+++ source/javascripts/_is-signed-in.js
@@ -1,7 +1,18 @@
-var elementsToShow = document.cookie.match(/isSignedIn/) ?
-  "[role=dashboard-link]" :
+var getCookie = function(name) {
+  match = document.cookie.match(new RegExp(name + '=([^;]+)'));
+  if (match) return match[1];
+}
+
+var accountEmail = getCookie('datoAccountEmail');
+
+var elementsToShow = accountEmail ?
+  "[role=dashboard-link], [role=account-email]" :
   "[role=sign-in-link], [role=register-link]";
 
 [].forEach.call(document.querySelectorAll(elementsToShow), function(el) {
   el.removeAttribute("style");
+});
+
+[].forEach.call(document.querySelectorAll("[role=account-email]"), function(el) {
+  el.innerText = accountEmail;
 });
