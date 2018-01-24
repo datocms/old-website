8e0d4d0a9121b1de870bb9196ea05e78e1d7674d
Stefano Verna <s.verna@cantierecreativo.net>
2018-01-24 17:02:04 +0100

    Websites

--- src/components/Browser/index.js
+++ src/components/Browser/index.js
@@ -4,13 +4,25 @@ import './style.sass'
 
 const b = bem.lock('Browser')
 
-export default ({ children, padded, inverse, ...props }) => (
-  <div className={b({ padded, inverse })}>
+export default ({ children, small, padded, title, address, inverse, ...props }) => (
+  <div className={b({ padded, inverse, small })}>
     <div className={b('title-bar')}>
       <div className={b('button')} />
       <div className={b('button')} />
       <div className={b('button')} />
+      {
+        title &&
+          <div className={b('title')}>
+            {title}
+          </div>
+      }
     </div>
+    {
+      address &&
+        <div className={b('address')}>
+          {address}
+        </div>
+    }
     <div className={b('content')}>
       { children }
     </div>
--- src/pages/use-cases.js
+++ src/pages/use-cases.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import Img from 'gatsby-image'
+import Masonry from 'react-masonry-component'
 
 import bem from 'utils/bem'
 import { Wrap, button, Space, text } from 'blocks'
 
-import Features from '../components/home/Features'
+import Browser from 'components/Browser'
 
 import './use-cases.sass'
 
@@ -14,6 +15,7 @@ const b = bem.lock('UseCasesPage')
 class UseCasesPage extends React.Component {
   render() {
     const { data } = this.props;
+    const websites = data.websites.edges.map(e => e.node);
 
     return (
       <div>
@@ -23,6 +25,40 @@ class UseCasesPage extends React.Component {
               Use Cases
             </div>
             <div className={b('content')}>
+              <Wrap>
+                <Masonry
+                  options={{
+                    columnWidth: '.UseCasesPage__grid-sizer',
+                    gutter: '.UseCasesPage__gutter-sizer',
+                    itemSelector: '.UseCasesPage__website',
+                    percentPosition: true
+                  }}
+                >
+                  <div className={b('grid-sizer')} />
+                  <div className={b('gutter-sizer')} />
+                  {
+                    websites.map((website, i) => (
+                      <a href={website.url} className={b('website', { [`type-${i % 2}`]: true })} key={website.id}>
+                        <Browser
+                          small
+                          title={website.title}
+                          address={website.url}
+                        >
+                            <div className={b('website-name')}>
+                              {website.title}
+                            </div>
+                            <div
+                              className={b('screen')}
+                              style={{
+                                backgroundImage: `url(${website.image.resize.src})`
+                              }}
+                            />
+                        </Browser>
+                      </a>
+                    ))
+                  }
+                </Masonry>
+              </Wrap>
             </div>
           </div>
         </Space>
@@ -33,3 +69,21 @@ class UseCasesPage extends React.Component {
 
 export default UseCasesPage
 
+export const query = graphql`
+query UseCasesQuery {
+  websites: allDatoCmsWebsite {
+    edges {
+      node {
+        id
+        title
+        url
+        image {
+          resize(width: 700, imgixParams: { fm: "jpg", q: 90 }) {
+            src
+          }
+        }
+      }
+    }
+  }
+}
+`
