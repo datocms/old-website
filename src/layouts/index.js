7a7290ba5268fca5806b3847728788e54614eb30
Stefano Verna <s.verna@cantierecreativo.net>
2018-01-31 09:48:39 +0100

    Fix

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,15 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-import { configureAnchors } from 'react-scrollable-anchor'
 
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 
 import './style.sass'
-
-// Offset all anchors by -60 to account for a fixed header
-configureAnchors({ offset: -100 })
 
 const TemplateWrapper = ({ location, children }) => (
   <div>
