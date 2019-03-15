25862d53f9a169d489f6272dfcbd68cc39fab1de
Matteo Giaccone <matteo@turnintocoders.it>
2019-03-15 16:26:33 +0100

    Fix 404s and improve 404 page

--- gatsby-node/redirects.js
+++ gatsby-node/redirects.js
@@ -1,28 +1,48 @@
 const redirectMappings = {
-  '/how-it-works/': '/docs/',
-  '/legal/privacy/':
+  '/how-it-works': '/docs',
+  '/legal/privacy':
     'https://www.iubenda.com/privacy-policy/64648824/full-legal',
-  '/legal/privacy-policy/':
+  '/legal/privacy-policy':
     'https://www.iubenda.com/privacy-policy/64648824/full-legal',
-  '/why-static/': '/',
-  '/gallery/': '/use-cases/',
+  '/why-static': '/',
+  '/gallery': '/use-cases',
   '/api/*': '/docs/content-management-api/:splat',
   '/content-management-api/*': '/docs/content-management-api/:splat',
   '/docs/graphql/*': '/docs/content-delivery-api/:splat',
-  '/docs/deployment/*': '/docs/depoyments/:splat',
+  '/docs/deployment/custom': '/docs/deployments/custom-webhook',
+  '/docs/deployment/*': '/docs/deployments/:splat',
   '/docs/import': '/docs/guides',
   '/docs/jekyll/*': '/docs/static-generators/jekyll/:splat',
   '/docs/gatsby/*': '/docs/static-generators/gatsbyjs/:splat',
   '/docs/hugo/*': '/docs/static-generators/hugo/:splat',
   '/docs/middleman/*': '/docs/static-generators/middleman/:splat',
   '/docs/metalsmith/*': '/docs/static-generators/metalsmith/:splat',
+  '/docs/other/*': '/docs/static-generators/other-ssg/:splat',
+  '/cms/spike/*': '/docs/static-generators/other-ssg',
   '/docs/plugins/*': '/docs/guides/building-plugins/:splat',
   '/docs/search/*': '/docs/guides/installing-site-search/:splat',
   '/docs/introduction/*': '/docs/general-concepts/:splat',
+  '/docs/backups': '/docs/guides/offline-backups',
+  '/docs/general-concepts/links': '/docs/content-modelling/links',
+  '/docs/general-concepts/trees': '/docs/content-modelling/trees',
+  '/docs/general-concepts/modular-content':
+    '/docs/content-modelling/modular-content',
+  '/docs/general-concepts/single-instance':
+    '/docs/content-modelling/single-instance',
+  '/docs/general-concepts/data-migration':
+    '/docs/content-modelling/data-migration',
+  '/docs/import/nodejs': '/docs/content-management-api/js-client',
+  '/docs/guides/building-plugins/introduction': '/docs/guides/building-plugins',
+  '/docs/static-websites': '/docs/general-concepts',
 };
 
 module.exports = function redirects({ actions: { createRedirect } }) {
   Object.entries(redirectMappings).forEach(([fromPath, toPath]) => {
-    createRedirect({ fromPath, toPath, isPermanent: true });
+    createRedirect({
+      fromPath,
+      toPath,
+      isPermanent: true,
+      redirectInBrowser: process.env.NODE_ENV == 'development',
+    });
   });
 };
--- src/pages/404.js
+++ src/pages/404.js
@@ -2,8 +2,14 @@ import React from 'react';
 import Layout from 'components/Layout';
 import { Wrap, Space } from 'blocks';
 
+import bem from 'utils/bem';
+
+import './404.sass';
+
 import PageLayout from 'components/PageLayout';
 import Search from 'components/Search';
+
+const b = bem.lock('Page404');
 
 const NotFoundPage = () => (
   <Layout>
@@ -11,11 +17,18 @@ const NotFoundPage = () => (
       <Wrap>
         <PageLayout
           noWrap
-          title="404 - Not found"
-          subtitle="Sorry! Maybe you can search something else:"
+          title="404 - This is not the page you are looking for."
+          subtitle="What you’re looking for seems to be missing. We’re definitely not using our Jedi mind trick to hide it. Yep."
           headerClass="Docs__header"
         >
-          <Search big />
+          <div className={b()}>
+            <Space bottom={1}>
+              <div className={b('search-title')}>
+                Maybe you can search for something else:
+              </div>
+            </Space>
+            <Search big />
+          </div>
         </PageLayout>
       </Wrap>
     </Space>
