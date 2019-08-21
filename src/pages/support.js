8d8b75e07842444201fc3c90d166d87178689993
Matteo Giaccone <matteo@turnintocoders.it>
2019-08-21 13:48:00 +0200

    Add form to support page

--- src/pages/support.js
+++ src/pages/support.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import Link from 'components/Link';
 import { graphql } from 'gatsby';
+import { HelmetDatoCms } from 'gatsby-source-datocms';
 
 import bem from 'utils/bem';
-import { Wrap, Space } from 'blocks';
+
+import { Space } from 'blocks';
 import Layout from 'components/Layout';
+import PageLayout from 'components/PageLayout';
 
 import './support.sass';
 
@@ -17,74 +19,102 @@ class Support extends React.Component {
   }
 
   render() {
-    const { authors } = this.props.data;
+    const page = this.props.data.datoCmsSupportPage;
 
     return (
       <Layout>
-        <Space both="10">
-          <Wrap>
-            <div className={b()}>
-              <div className={b('left')}>
-                <div className={b('logo')}>
-                  {authors.edges.map(({ node: author }) => (
-                    <img
-                      alt={author.name}
-                      src={`${author.avatar.url}?w=300&h=300&fit=crop&crop=faces`}
-                      className={b('person')}
-                      key={author.name}
-                    />
-                  ))}
-                </div>
-                <div className={b('title')}>We're here to help!</div>
-
-                <div className={b('content')}>
-                  <Space bottom={2}>
-                    Start in our{' '}
-                    <Link
-                      to="https://community.datocms.com"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      community forum
-                    </Link>{' '}
-                    searching for existing information or asking your question.
-                  </Space>
-
-                  <Space bottom={2}>
-                    Check our{' '}
-                    <Link
-                      to="https://status.datocms.com"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      status page
-                    </Link>{' '}
-                    to get a full picture on how our systems are going and on their performance on the past month.
-                  </Space>
-
-                  <Space bottom={2}>
-                    For account specific questions or commercial inquiries you can use{' '}
-                    <button onClick={this.handleOpenChat.bind(this)}>
-                      the chat widget
-                    </button>{' '}
-                    or send us a mail at{' '}
-                    <a href="mailto:support@datocms.com">support@datocms.com</a>,
-                    we'll get back to you as soon as possible.
-                  </Space>
-
-                  You can also find help in our awesome{' '}
-                  <Link to="/slack/">Slack channel</Link>.
-                </div>
-                <div className={b('info')}>
-                  Higher priority will be given to customers with plans with
-                  guarantees of faster response times.
-                </div>
-              </div>
+        <HelmetDatoCms seo={page.seoMetaTags} />
+        <PageLayout title={page.title}>
+          <div className={b()}>
+            <div className={b('people')}>
+              {page.people.map((person) => (
+                <img
+                  alt={person.name}
+                  src={`${person.avatar.url}?w=300&h=300&fit=crop&crop=faces`}
+                  className={b('person')}
+                  key={person.name}
+                />
+              ))}
             </div>
-          </Wrap>
-        </Space>
+
+            <div className="content-body">
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: page.content.markdown.html,
+                }}
+              />
+              
+              <h2>{page.formTitle}</h2>
+
+              <Space bottom={2}>
+                <form
+                  method="POST"
+                  name="fa-form-1"
+                  action={page.formAction}
+                  enctype="multipart/form-data"
+                  accept-charset="utf-8"
+                  className="form"
+                >
+                  <fieldset className="fieldset">
+                    {page.form.map((field) => (
+                      <div class="form__field">
+                        <label for={`_${field.inputName}`} class="form__label">
+                          {field.label}
+                          {field.required && (
+                            <span class="form__field__required"> Required</span>
+                          )}
+                        </label>
+                        {this.renderField(field)}
+                        <div class="form__hint">{field.hint}</div>
+                      </div>
+                    ))}
+
+                    <div className={b('form__send')}>
+                      <input type="submit" value="Send" className="button button--red button--normal-big button--expand" />
+                    </div>
+                  </fieldset>
+                </form>
+              </Space>
+
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: page.bottomContent.markdown.html,
+                }}
+              />
+            </div>
+          </div>
+        </PageLayout>
       </Layout>
     );
+  }
+
+  renderField(field) {
+    switch(field.inputType) {
+      case 'text':
+      case 'email':
+        return (
+          <input id={`${field.inputName}`} name={`${field.inputName}`} placeholder={field.placeholder} size="30" type={field.inputType} required={field.required} />
+        )
+      case 'textarea':
+        return (
+          <textarea cols="40" id={`${field.inputName}`} name={`${field.inputName}`} rows="15" required={field.required}></textarea>
+        )
+      case 'file':
+        return (
+          <input id={`${field.inputName}`} name={`${field.inputName}`} type="file" multiple="" required={field.required} />
+        )
+      case 'select':
+        return (
+          <select id={`${field.inputName}`} name={`${field.inputName}`} required={field.required}>
+            <option value="">Please select one...</option>
+            {field.placeholder.split('\n').map(e => e.split('|')).map( entry => (
+              <option value={entry[0]}>{entry[1]}</option>
+            ))}
+          </select>
+        )
+      default:
+        break;
+    }
   }
 }
 
@@ -92,14 +122,36 @@ export default Support;
 
 export const query = graphql`
   query SupportQuery {
-    authors: allDatoCmsTeamMember {
-      edges {
-        node {
-          name
-          avatar {
-            url
-          }
+    datoCmsSupportPage {
+      title
+      people {
+        name
+        avatar {
+          url
         }
+      }
+      content: contentNode {
+        markdown: childMarkdownRemark {
+          html
+        }
+      }
+      formTitle
+      formAction
+      form {
+        label
+        placeholder
+        hint
+        inputType
+        inputName
+        required
+      }
+      bottomContent: bottomContentNode {
+        markdown: childMarkdownRemark {
+          html
+        }
+      }
+      seoMetaTags {
+        ...GatsbyDatoCmsSeoMetaTags
       }
     }
   }
