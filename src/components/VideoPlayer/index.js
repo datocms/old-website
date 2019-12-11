240b2907411ed6427c81835ca2c85a225c87fcea
Stefano Verna <s.verna@datocms.com>
2019-12-11 11:03:44 +0100

    Styling

--- src/components/VideoPlayer/index.js
+++ src/components/VideoPlayer/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import videojs from 'video.js';
 import 'video.js/dist/video-js.css';
+import './style.sass'
 
 const Controls = {
   "play": "playToggle",
